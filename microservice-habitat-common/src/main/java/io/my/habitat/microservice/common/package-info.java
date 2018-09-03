ba96d4cfda12c5515fe26c3087aaba93da46b98c
Austin Rowsell <andyrow123@hotmail.co.uk>
2018-09-03 21:05:07 +0100

    Added Base files and config.

---
+++ microservice-habitat-common/src/main/java/io/my/habitat/microservice/common/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Indicates that this module contains classes that need to be generated / processed.
+ */
+@ModuleGen(name = "my-habitat-common", groupPackage = "io.my.habitat.microservice.common")
+
+package io.my.habitat.microservice.common;
+
+import io.vertx.codegen.annotations.ModuleGen;
